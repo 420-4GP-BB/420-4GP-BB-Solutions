458e35fd7d13404a548e12a27acd11236633da2f
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2022-03-13 22:42:18 -0400

    H2022: Module 5
    Quand le joueur entre en contact avec un champignon, il perd.

--- Solutions-Module5/Exercice2_3/Assets/Scripts/ContactJoueur.cs
+++ Solutions-Module5/Exercice2_3/Assets/Scripts/ContactJoueur.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Classe qui vérifie si le champignon entre en contact avec le joueur.
+/// Si c'est le cas, le joueur perd la partie.
+/// 
+/// Auteur: Éric Wenaas
+/// </summary>
 public class ContactJoueur : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// Le joueur
+    /// </summary>
+    private GameObject joueur;
+
     void Start()
     {
-        
+        joueur = GameObject.Find("Joueur");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject == joueur)
+        {
+            SceneManager.LoadScene("Defaite");
+        }
     }
 }
