e54495b53fcb544646cd49b74b04bda2ad69a500
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2022-04-24 23:22:21 -0400

    H2022:  Module 7
    Ajout d'un test.

--- Solutions-Module7/M7_Exercices/Assets/PlayTests/dommage_projectile.cs
+++ Solutions-Module7/M7_Exercices/Assets/PlayTests/dommage_projectile.cs
@@ -38,4 +38,36 @@ public class dommage_projectile
         // ASSERT
         Assert.AreEqual(2, pdv.NombrePointsVie);
     }
+
+    [UnityTest]
+    public IEnumerator projectile_detruit_apres_collision()
+    {
+        // ARRANGE
+        GameObject monstre = new GameObject();
+        PointDeVie pdv = monstre.AddComponent<PointDeVie>();
+        pdv.Construct(3);
+
+        BoxCollider collider = monstre.AddComponent<BoxCollider>();
+        collider.size = new Vector3(1, 1, 1);
+
+        GameObject projectile = new GameObject();
+        projectile.AddComponent<Rigidbody>();
+        DommageProjectile dommage = projectile.AddComponent<DommageProjectile>();
+        dommage.Construct(1);
+
+        SphereCollider sphereCollider = projectile.AddComponent<SphereCollider>();
+        sphereCollider.radius = 1.0f;
+
+
+        // ACT
+        yield return null;
+        monstre.transform.position = projectile.transform.position;
+        yield return new WaitForFixedUpdate();
+
+        // Ne passe pas toujours sans ça... je crois
+        yield return new WaitForSeconds(0.3f);
+
+        // ASSERT
+        Assert.IsTrue(projectile == null);
+    }
 }
