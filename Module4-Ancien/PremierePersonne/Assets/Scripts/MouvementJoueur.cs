fe1c52629a8a95f2b07438a0b46bf9dc7cda8f25
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2023-03-07 10:20:51 -0500

    H2023: Passage à 2021.3.16f1
    OnControllerColliderHit fonctionne correctement.

--- Module4-Ancien/PremierePersonne/Assets/Scripts/MouvementJoueur.cs
+++ Module4-Ancien/PremierePersonne/Assets/Scripts/MouvementJoueur.cs
@@ -1,10 +1,10 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// Permet de déplacer un joueur avec un CharacterController
+/// Le saut et la chute sont implémentés
 /// </summary>
 public class MouvementJoueur : MonoBehaviour
 {
@@ -20,30 +20,88 @@ public class MouvementJoueur : MonoBehaviour
     /// Le facteur de multiplication pour la course
     /// </summary>
     private float augmentationCourse;
+    /// <summary>
+    /// Une simulation de la gravité
+    /// </summary>
+    private float gravite;
+    /// <summary>
+    /// Une simulation de saut vers le haut
+    /// </summary>
+    private float impulsion;
+    /// <summary>
+    /// La position initiale du joueur
+    /// </summary>
+    private Vector3 positionInitiale;
+    /// <summary>
+    /// La rotation initiale du joueur
+    /// </summary>
+    private Quaternion rotationInitiale;
+    /// <summary>
+    /// Le facteur d'accélération pour la course
+    /// </summary>
+    private float facteurCourse;
+    /// <summary>
+    /// La vélocity est utilisée pour simuler un effet de gravité
+    /// </summary>
+    private Vector3 velocity;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         augmentationCourse = 1.5f;
+        gravite = -9.8f;
+        impulsion = 1.0f;
+        positionInitiale = transform.position;
+        rotationInitiale = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * vitesse;
-        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * vitesse;
-        
-        // On regarde si on court
-        if (Input.GetKey(KeyCode.LeftShift))
+        bool groundedPlayer = characterController.isGrounded;
+        float horizontal = 0.0f;
+        float vertical = 0.0f;
+
+        horizontal = Input.GetAxis("Horizontal") * vitesse * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * vitesse * Time.deltaTime;
+
+
+        // Si on est sur le sol, on ne doit plus descendre
+        if (groundedPlayer && velocity.y < 0)
         {
-            horizontal *= augmentationCourse;
-            vertical *= augmentationCourse;
+            velocity.y = 0f;
         }
 
-        // Déplacement du personnage
-        Vector3 translation = new Vector3(horizontal, 0, vertical);
-        translation = transform.TransformDirection(translation);
-        characterController. Move(translation);
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        direction = transform.TransformDirection(direction);
+        
+        characterController.Move(direction);
+
+        if (velocity.y == 0 && Input.GetButtonDown("Jump"))
+        {
+            velocity.y += Mathf.Sqrt(impulsion * -3.0f * gravite);
+        }
+
+        velocity.y += gravite * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Méthode qui replace le joueur dans sa position et sa rotation initiale
+    /// </summary>
+    internal void ReplacerJoueur()
+    {
+        characterController.enabled = false;
+        transform.position = positionInitiale;
+        transform.rotation = rotationInitiale;
+        characterController.enabled = true;
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject == GameObject.Find("Objectif"))
+        {
+            ReplacerJoueur();
+        }
     }
 }
