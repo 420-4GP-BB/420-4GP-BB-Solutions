21a2f2b2bae256fe1ad5e1a47cad329d699a0e84
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2024-04-18 17:16:06 -0400

    Version de synchronisation

---
+++ Module8/TestSerialisation/Assets/DeplacerCube.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeplacerCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(2, 2, 2);
+    }
+}
