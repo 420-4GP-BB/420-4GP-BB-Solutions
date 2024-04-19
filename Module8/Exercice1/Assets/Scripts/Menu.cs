0e65b94780234ae178b4b446fd6ab01965ee06ee
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2024-04-19 14:34:12 -0400

    Bouton Charger se désactive quand le fichier de sauvegarde est absent.

--- Module8/Exercice1/Assets/Scripts/Menu.cs
+++ Module8/Exercice1/Assets/Scripts/Menu.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    private Button _boutonCharger;
+
+    public void Start()
+    {
+        _boutonCharger = GameObject.Find("BoutonCharger").GetComponent<Button>();
+        _boutonCharger.interactable = GetComponent<GestionnaireSauvegarde>().FichierExiste;
+    }
 
     public void ChargerPartie()
     {
